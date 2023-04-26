dca9d0cfc9e4ee23d02c0c56dbd9fc877958a70a
Wanna-be-tech-guy <andy1988c@gmail.com>
2023-04-26 02:18:48 -0700

    if/elif indent change (lines 161-170)

--- Network_tool/Network_Troubleshooting_Tool.py
+++ Network_tool/Network_Troubleshooting_Tool.py
@@ -158,16 +158,16 @@ while True:
                         print("Nmap scan report for", nmap_user_ip_list[0])
                         print(host_line)
                         print(status_line)
-                    elif len(nmap_user_ip_list) > 2:
-                        for ip in nmap_user_ip_list:
-                            nmap = subprocess.run(["nmap", "-sn", nmap_user_ip_list], capture_output=True, text=True)
-                            if nmap.returncode == 0:
-                                output_lines = nmap.stdout.splitlines()
-                                host_line = output_lines[1]
-                                status_line = output_lines[2]
-                                print("Nmap scan for", nmap_user_ip_list)
-                                print(host_line)
-                                print(status_line)
+                elif len(nmap_user_ip_list) > 2:
+                    for ip in nmap_user_ip_list:
+                        nmap = subprocess.run(["nmap", "-sn", nmap_user_ip_list], capture_output=True, text=True)
+                        if nmap.returncode == 0:
+                            output_lines = nmap.stdout.splitlines()
+                            host_line = output_lines[1]
+                            status_line = output_lines[2]
+                            print("Nmap scan for", nmap_user_ip_list)
+                            print(host_line)
+                            print(status_line)
             elif nmap_menu_selction.strip() == "0":
                 break
             elif nmap_menu_selction.strip() == "00":
