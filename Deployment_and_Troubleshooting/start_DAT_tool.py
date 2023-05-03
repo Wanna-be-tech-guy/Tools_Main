25735f8e2d66c39c9bdf6b951b27ae2029fb77a0
Wanna-be-tech-guy <andy1988c@gmail.com>
2023-05-03 14:06:56 -0700

    (hopefully) final test

--- Deployment_and_Troubleshooting/start_DAT_tool.py
+++ Deployment_and_Troubleshooting/start_DAT_tool.py
@@ -5,4 +5,4 @@ subprocess.run(["git", "pull"])
 
 time.sleep(10)
 
-subprocess.run(["usr/bin/python3", "deployment_and_troubleshooting_tool..py"])
+subprocess.run(["usr/bin/python3", "deployment_and_troubleshooting_tool.py"])
