82fd1ba95869184b69e3a565f45eae67058663a2
Wanna-be-tech-guy <andy1988c@gmail.com>
2023-05-03 11:46:21 -0700

    added dropdown line to menu options statement..

--- Deployment_and_Troubleshooting/deployment_and_troubleshooting_tool.py
+++ Deployment_and_Troubleshooting/deployment_and_troubleshooting_tool.py
@@ -5,7 +5,7 @@ clean()
 welcome_message()
 
 while True:
-    menu_selction = input("Menu Options:\n1.Set-up (Should only need ran once, or on a new instance)\n2.Ping\n3.NMAP scan(s)\n4.TraceRoute\n5.Manage Resources\n6.Clear screen\n00.Exit")
+    menu_selction = input("Menu Options:\n1.Set-up (Should only need ran once, or on a new instance)\n2.Ping\n3.NMAP scan(s)\n4.TraceRoute\n5.Manage Resources\n6.Clear screen\n00.Exit\nEnter selection: ")
     if menu_selction.strip() == "1" or menu_selction.lower() == "set-up" or menu_selction.lower() == "set up":
         feature_not_added() #Change this once the other menu and functions have been built!
     elif menu_selction.strip() == "2" or menu_selction.lower() == "ping":
