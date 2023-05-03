0f462321c47ad43a63c15bc4870d8745c2010d8f
Wanna-be-tech-guy <andy1988c@gmail.com>
2023-05-03 14:26:35 -0700

    more testing

--- Deployment_and_Troubleshooting/start_DAT_tool.py
+++ Deployment_and_Troubleshooting/start_DAT_tool.py
@@ -1,8 +1,6 @@
 import subprocess
 import time
+from deployment_and_troubleshooting_tool import *
 
 subprocess.run(["git", "pull"])
-
-time.sleep(10)
-
 subprocess.run(["usr/bin/python3", "deployment_and_troubleshooting_tool.py"], cwd="/home/linux-user/gitmaster/Network_Troubleshooting_Tool/https---github.com-Wanna-be-tech-guy-Network_Troubleshooting_Tool/Deployment_and_Troubleshooting")
